1172485cb06ef09b17bb41e9168cc49bb4bf7e66
Lautaro <lautarosoliani@hotmail.com>
2021-08-15 02:06:28 -0300

    refactoring

---
+++ taskineitor/src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+import Filtros from "./components/Filtros";
+import Header from "./components/Header";
+import Resultados from "./components/Resultados";
+import "bootstrap";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Filtros />
+      <Resultados />
+    </>
+  );
+}
