61f8221254bcfc95c23a19ff6954aa09ea878864
Lautaro <lautarosoliani@hotmail.com>
2021-08-31 09:08:29 -0300

    new components

--- taskineitor/src/App.js
+++ taskineitor/src/App.js
@@ -1,15 +1,15 @@
-import "./styles.css";
-import Filtros from "./components/Filtros";
-import Header from "./components/Header";
-import Resultados from "./components/Resultados";
-import "bootstrap";
+import "./styles.css"
+import Filtros from "./components/Filtros"
+import Header from "./components/Header"
+import Resultados from "./components/Resultados"
+import "bootstrap"
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <Filtros />
-      <Resultados />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Filtros />
+            <Resultados />
+        </>
+    )
 }
--- taskineitor/src/components/Header.jsx
+++ taskineitor/src/components/Header.jsx
@@ -1,16 +1,15 @@
+import "./Header.css"
 const Header = () => {
-  return (
-    <div className="header">
-      <div className="contenedor-header">
-        <h1 className="flex-start">Taskinator</h1>
-        <div className="contenedor-placeholder">
-          <input placeholder="Texto placeholder" />
-          <input placeholder="Texto placeholder" />
-          <input placeholder="Texto placeholder" />
+    return (
+        <div className="header">
+            <div className="contenedor-header">
+                <h1 className="flex-start">Taskinator</h1>
+                <div className="contenedor-placeholder">
+                    <input placeholder="Your new task here!" />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Header;
+export default Header
